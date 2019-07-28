5843f094bc42e4735ee6438ee2acae0d0b36b48e
Tomi Karikj <karikjtomi@gmail.com>
2019-07-28 18:59:51 +0200

    Added the production related rules and logic

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/events', require('./routes/events'));
 app.use('/api/auth', require('./routes/auth'));
 
-// if (process.env.NODE_ENV === 'production') {
-//   // Set the static folder
-//   app.use(express.static('client'));
+if (process.env.NODE_ENV === 'production') {
+  // Set the static folder
+  app.use(express.static('client/build'));
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
-//   });
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
